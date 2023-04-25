794ce3e5f47eb7c6a196fefd0cc860d7dcf5e415
Abiola David <davidolanrewaju2000@gmail.com>
2023-04-25 14:14:06 +0100

    Enforced unique email constraint on user signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,38 @@
-import { Injectable } from "@nestjs/common"; 
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaDbService } from "src/prisma-db/prisma-db.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
+import { Prisma } from "@prisma/client";
 
 @Injectable({})
-export class AuthService{
-    constructor(private prisma: PrismaDbService){}
+export class AuthService {
+    constructor(private prisma: PrismaDbService) { }
 
-    async signUp(dto: AuthDto){
-        const hash = await argon.hash(dto.password)
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hashPassword: hash
+    async signUp(dto: AuthDto) {
+        try {
+            const hash = await argon.hash(dto.password)
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hashPassword: hash
+                }
+            })
+            delete user.hashPassword
+            return user
+        }
+
+        catch (error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException("Credentials already taken")
+                }
             }
-        })
-        delete user.hashPassword
-        return user
+
+            throw error
+        }
     }
 
-    logIn(){
+    logIn() {
         return {
             "message": "Sign in endpoint"
         }
